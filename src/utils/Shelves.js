ea2dc6aae744548daf9208d539bab356f6b2c866
Fernando Goerl <fernandogoerl@gmail.com>
2018-06-05 18:50:04 -0300

    feat: shelves data
    JSON to help manage shelves

---
+++ src/utils/Shelves.js
@@ -0,0 +1,17 @@
+export const Shelves = {
+	readingShelf: {
+		id: 1,
+		name: 'currentlyReading',
+		displayName: 'Reading'
+	},
+	wantToReadShelf: {
+		id: 2,
+		name: 'wantToRead',
+		displayName: 'To Read'
+	},
+	readShelf: {
+		id: 3,
+		name: 'read',
+		displayName: 'Read'
+	}
+};
