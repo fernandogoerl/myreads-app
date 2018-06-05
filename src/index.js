cacf292bf950634e51e1f77de7647d0f1bda0fc9
Fernando Goerl <fernandogoerl@gmail.com>
2018-06-05 18:46:39 -0300

    refactor: remove ServiceWorker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <BrowserRouter><App /></BrowserRouter>,
+    document.getElementById('root')
+);
